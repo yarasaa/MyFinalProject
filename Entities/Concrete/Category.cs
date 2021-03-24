eb13b39cbeccd5cd49efb2f46caf1640129cf0cc
Mehmet Akbaba <mehmet.akbaba2011@gmail.com>
2021-03-25 00:03:53 +0300

    Add project files.

---
+++ Entities/Concrete/Category.cs
@@ -0,0 +1,14 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Concrete
+{
+    //Bir Class herhangi bir inheritance interface almıyorsa sorun vardır.
+    public class Category:IEntity
+    {
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+    }
+}
