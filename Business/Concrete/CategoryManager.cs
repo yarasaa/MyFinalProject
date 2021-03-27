1d5c7e32bbe38a1000c1efba5f22fb46553021ce
Mehmet Akbaba <mehmet.akbaba2011@gmail.com>
2021-03-27 17:07:19 +0300

    EntityRepositoryBase oluşturuldu

---
+++ Business/Concrete/CategoryManager.cs
@@ -0,0 +1,30 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+   public class CategoryManager
+    {
+        ICategoryDal _categoryDal;
+
+        public CategoryManager(ICategoryDal categoryDal)
+        {
+            _categoryDal = categoryDal;
+        }
+
+        public List<Category> GetAll()
+        {
+            return _categoryDal.GetAll();
+        }
+
+        public Category GetById(int categoryId)
+        {
+            // bu kod aslında bunu arka planda yapar;
+            //Selecet *from Categories where CategoryId=3;
+            return _categoryDal.Get(c => c.CategoryId == categoryId);
+        }
+    }
+}
---
+++ Core/DataAccess/IEntityRepository.cs
@@ -0,0 +1,24 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace Core.DataAccess
+{ //Core katmanı diğer katmanları referans almaz.
+  //Generic constraint generic kısıt
+  //Class: referans tip olabilir demek.
+  //                       where T: şartı yazarak kısıtlama getirdik.
+  // IEntity: IEntity olabilir veya IEntity implemente eden bir nesne olabilir demek.
+  //New() yazdık çünkü IEntity kullanılmasını istemiyoruz 
+  //Ientity implemente eden classlar kullanılsın istiyoruz sadece
+  //o yüzden IEntity newlenemediği için New() şartı ekledik
+    public interface IEntityRepository<T> where T:class,IEntity,new()
+    {
+        List<T> GetAll(Expression<Func<T,bool>> filter=null);
+        T Get(Expression<Func<T, bool>> filter);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+    }
+}
