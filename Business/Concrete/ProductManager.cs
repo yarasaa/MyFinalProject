a4f59f40b3a70255e4b49eedc3da3eb48a62e3e4
Mehmet Akbaba <mehmet.akbaba2011@gmail.com>
2021-04-07 00:43:59 +0300

    Jwt entegrasyonu

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -1,4 +1,5 @@
 ﻿using Business.Abstract;
+using Business.BusinessAspect.Autofac;
 using Business.CCS;
 using Business.Constants;
 using Business.ValidationRules.FluentValidation;
@@ -33,6 +34,7 @@ namespace Business.Concrete
         }
          
         [ValidationAspect(typeof(ProductValidator))]
+        [SecuredOperation("product.add,admin")]
         public IResult Add(Product product)
         {
              IResult result=   BusinessRules.Run(CheckIfProductNameExists(product.ProductName),
--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -1,6 +1,8 @@
-﻿using Entities.Concrete;
+﻿using Core.Entities.Concrete;
+using Entities.Concrete;
 using System;
 using System.Collections.Generic;
+using System.Runtime.Serialization;
 using System.Text;
 
 namespace Business.Constants
@@ -15,5 +17,14 @@ namespace Business.Constants
         public static string ProductCountOfCategoryError = "Bir kategoride en fazla on ürün olabilir";
         public static string ProductNameAlreadyExists = "Bu isimde başka bir ürün var";
         public static string CategoryLimitExceded = "KAtegori Limiti aşıldığı için ürün eklenemiyor";
+        public static string UserNotFound = "Kullanıcı bulunamadı";
+        public static string PasswordError = "Şifre hatalı";
+        public static string SuccessfulLogin = "Sisteme giriş başarılı";
+        public static string UserAlreadyExists = "Bu kullanıcı zaten mevcut";
+        public static string UserRegistered = "Kullanıcı başarıyla kaydedildi";
+        public static string AccessTokenCreated = "Access token başarıyla oluşturuldu";
+
+        public static string AuthorizationDenied = "Yetkiniz yok";
+        
     }
 }
