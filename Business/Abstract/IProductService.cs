1764427c4990d36be4fccd5fe105bd81208d1b62
Mehmet Akbaba <mehmet.akbaba2011@gmail.com>
2021-04-08 16:11:58 +0300

    Performance,Caching,Transaction desteği eklendi.

--- Business/Abstract/IProductService.cs
+++ Business/Abstract/IProductService.cs
@@ -16,5 +16,6 @@ namespace Business.Abstract
         IResult Add(Product product);
         IDataResult<Product> GetById(int productId);
         IResult Update(Product product);
+        IResult AddTransactionalTest(Product product);
     }
 }
--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using Business.Abstract;
 using Business.Concrete;
+using Core.DependencyResolvers;
+using Core.Extensions;
+using Core.Utilities.IoC;
 using Core.Utilities.Security.Encryption;
 using Core.Utilities.Security.JWT;
 using DataAccess.Abstract;
@@ -11,6 +14,7 @@ using DataAccess.Concrete.EntityFramework;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
@@ -55,8 +59,7 @@ namespace WebAPI
                         IssuerSigningKey = SecurityKeyHelper.CreateSecurityKey(tokenOptions.SecurityKey)
                     };
                 });
-
-
+            services.AddDependencyResolvers(new ICoreModule[] {new CoreModule() });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
